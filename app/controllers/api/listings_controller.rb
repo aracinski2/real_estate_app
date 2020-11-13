b17be42173dc07ca7786866766eb65c88587a7ab
Alex Racinski <aracinski2@gmail.com>
2020-11-13 13:41:51 -0600

    add geocoder logic to create action

--- app/controllers/api/listings_controller.rb
+++ app/controllers/api/listings_controller.rb
@@ -1,5 +1,9 @@
 class Api::ListingsController < ApplicationController
   def create
+    results = Geocoder.search(params[:address])
+    the_latitude = results.first.coordinates[0]
+    the_longitude = results.first.coordinates[1]
+
     @listing = Listing.new(
       description: params[:description],
       year_built: params[:year_built],
@@ -9,8 +13,8 @@ class Api::ListingsController < ApplicationController
       floors: params[:floors],
       availability: params[:availability],
       price: params[:price],
-      latitude: params[:latitude],
-      longitude: params[:longitude]
+      latitude: the_latitude,
+      longitude: the_longitude
     )
     @listing.save
     render 'show.json.jb'
